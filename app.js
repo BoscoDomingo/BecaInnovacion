dbeca80ea68968964f6745b392c2f56bbc502843
Bosco <boscodomingob@gmail.com>
2019-04-19 20:42:51 +0200

    Dummy Login and Sign Up
    Need to implement inserting into DB and checking against said DB

--- app.js
+++ app.js
@@ -3,43 +3,46 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
-let hbs = require('express-hbs');
+let hbs = require('express-hbs'),
+    expressValidator = require('express-validator'),
+    expressSession = require('express-session');
+require('dotenv').config(); //loads .env file
 
 let routes = require('./routes/routes.js');
 
 let app = express();
 
 // view engine setup
-app.engine('hbs', hbs.express4({ defaultLayout: __dirname + '/views/layouts/layout.hbs' }));
+app.engine('hbs', hbs.express4({ layoutsDir: __dirname + "/views/layouts", defaultLayout: null })); //defaultLayout: __dirname + '/views/layouts/layout.hbs'
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: false
-}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public'))); //serves static files from a certain dir under url .com/filename
 //app.use('/static', express.static(path.join(__dirname, 'public')));//serves them under the url .com/static/filename
+app.use(expressValidator());
+app.use(expressSession({ secret: process.env.SECRET, saveUninitialized: false, resave: false }));
 
 app.use('/', routes); //defined above
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/routes.js
+++ routes/routes.js
@@ -1,33 +1,117 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'English For Professional and Academic Communication extra credit page',
-    layout: null
-  });
+    res.render('index', {
+        title: 'English For Professional and Academic Communication extra credit page',
+        layout: null
+    });
 });
 
-router.get('/loginStudent', function (req, res, next) {
-  res.render('loginS', {
-    title: 'Login',
-    layout: null
-  });
+router.get('/student-sign-up', function (req, res, next) {
+    res.render('signUpS', {
+        title: 'Sign up',
+        layout: 'signUpLayout',
+        success: req.session.success,
+        errors: req.session.errors
+    });
+    req.session.errors = null;
+    req.session.success = null;
 });
 
-router.get('/loginTeacher', function (req, res, next) {
-  res.render('loginT', {
-    title: 'Login',
-    layout: null
-  });
+router.post('/student-sign-up/submit', function (req, res, next) {
+    req.check('password', 'Invalid password').isLength({ min: 8 }).equals(req.body.confirmPassword);
+    req.check('email', 'Invalid email address').isEmail();
+    let errors = req.validationErrors()
+    if (errors) {
+        req.session.errors = errors;
+        req.session.success = false;
+    }
+    res.redirect('/student-login');
+
 });
 
+router.get('/student-login', function (req, res, next) {
+    res.render('loginS', {
+        title: 'Login',
+        layout: 'loginLayout',
+        success: req.session.success,
+        errors: req.session.errors
+    });
+    req.session.errors = null;
+    req.session.success = null;
+});
+
+router.post('/student-login/submit', function (req, res, next) {
+    req.check('password', 'Invalid password').isLength({ min: 8 }).equals(req.body.confirmPassword);
+    req.check('email', 'Invalid email address').isEmail();
+    let errors = req.validationErrors()
+    if (errors) {
+        req.session.errors = errors;
+        req.session.success = false;
+    }
+    res.redirect('/student-section');
+});
+
+router.get('/teacher-sign-up', function (req, res, next) {
+    res.render('signUpT', {
+        title: 'Sign up',
+        layout: 'signUpLayout',
+        success: req.session.success,
+        errors: req.session.errors
+    });
+    req.session.errors = null;
+    req.session.success = null;
+});
+
+router.get('/teacher-login', function (req, res, next) {
+    res.render('loginT', {
+        title: 'Login',
+        layout: 'loginLayout',
+        success: req.session.success,
+        errors: req.session.errors
+    });
+    req.session.errors = null;
+    req.session.success = null;
+});
+
+router.get('/student-section', function (req, res, next) {
+    res.render('homePageS', {
+        title: 'Home Page',
+        layout: 'menuLayout'
+    });
+});
 //URL: localhost:port/users/
 /* GET users listing. */
-// router.get('/', function (req, res, next) {
-//   res.send('respond with a resource');
+// router.get('/users', function (req, res, next) {
+//     res.render(...);
+//     res.redirect('url path');
 // });
 
+let mysql = require('mysql');
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: process.env.db_user_S,
+    password: process.env.db_pass_S,
+    database: 'test-db'
+});
+
+connection.connect((err) => {
+    if (err) console.log(err);
+})
+
+connection.query('SELECT * FROM students', function (err, rows, fields) {
+    if (err) throw err;
+    console.log('The solution is: ', rows[0])
+})
+
+connection.end()
+
+//Sources: https://o7planning.org/en/11959/connecting-to-mysql-database-using-nodejs
+//https://appdividend.com/2018/08/25/how-to-connect-nodejs-application-to-mysql-database/
 
 module.exports = router;
