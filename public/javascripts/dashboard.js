9e96217bec3974101ea6f2f64ddf6a664310cbde
Bosco <boscodomingob@gmail.com>
2019-06-25 20:28:39 +0200

    Implementation of Lists.js in Dashboard

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,1 +1,16 @@
-'use strict'
+document.addEventListener("DOMContentLoaded", () => {
+    const ownActivityListContainer = document.getElementById('own_activity_list'),
+        completedActivityListContainer = document.getElementById('completed_activity_list');
+
+    var listOptions = {
+        valueNames: ['title', 'id', 'author', 'category', 'tags'],
+    };
+
+    var activityList = new List('activity_list', listOptions);
+    if (completedActivityListContainer) {
+        var completedActivityList = new List(completedActivityListContainer, listOptions);
+    } else if (ownActivityListContainer) {
+        var ownActivityList = new List(ownActivityListContainer, listOptions);
+    }
+});
+
