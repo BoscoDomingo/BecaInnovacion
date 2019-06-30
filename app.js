4279797dad38139a810121efcb9b66ee49218239
Luis <luisdemarcos1999@gmail.com>
2019-06-30 17:45:42 +0200

    CSS: ranking change + minor fixes

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(session({
     cookie: {
         maxAge: parseInt(process.env.SESS_LIFETIME), //ms before expiration after last request
         sameSite: true, //cookies only work in our domain
-        secure: INPROD //they will require HTTPS
+        // secure: INPROD //they will require HTTPS
     }
 }));
 
@@ -80,13 +80,13 @@ app.use((req, res, next) => {
 app.use('/', routes); //defined above
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-    if(!INPROD){
+app.use(function(err, req, res, next) {
+    if (!INPROD) {
         // set locals, only providing error in development
         res.locals.message = err.message;
         res.locals.error = !INPROD ? err : {}; //res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -95,17 +95,17 @@ app.use(function (err, req, res, next) {
         // render the error page
         res.render('error');
 
-    //redirect to main page
-    //res.redirect('/');
-    }else{
+        //redirect to main page
+        //res.redirect('/');
+    } else {
         // render the error page
         res.render('errorProd');
     }
-    
+
 });
 
 //response headers
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
