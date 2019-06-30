510ae1509373a74f3631f541f0a60b307d309024
Bosco Domingo <boscodomingob@gmail.com>
2019-06-30 20:17:34 +0200

    Password Generator

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,5 +1,16 @@
 /*
 Simply input the new temporary password into the update() field, inside single (' ') or double(" ") quotes.
-Then paste the result of running 'node passwordGenerator' inside the student's "password" field in the database and apply
-*/
-console.log(require('crypto').createHash('sha256').update('Student6').digest('base64'));
+Then paste the result of running 'node passwordGenerator' inside the student's "password" field in the database and apply.
+
+You can also use this generator to create the password by using update(generateOTP()) instead:*/
+function generateOTP() {
+    const string = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var OTP = '',
+        len = string.length;
+    for (let i = 0; i < 8; i++) {
+        OTP += string[Math.floor(Math.random() * len)];
+    }
+    console.log("\nThe password the user has to input is: " + OTP);
+    return OTP;
+}
+console.log(`\nThe password to paste into DB is: ${require('crypto').createHash('sha256').update(generateOTP()).digest('base64')}\n`);
