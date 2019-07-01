41a65433f20a4db0d15d5d92de4c28ea55a39112
Bosco Domingo <boscodomingob@gmail.com>
2019-07-01 16:11:19 +0200

    Updated passwordGenerator

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,6 +1,8 @@
 /*
+Node.js is required for this to work. Either paste this code in a "playground" website that has it like https://repl.it/languages/nodejs and press "Run"
+or https://www.katacoda.com/courses/nodejs/playground and enter "node app", or install the current version from https://nodejs.org/en/.
 Simply input the new temporary password into the update() field, inside single (' ') or double(" ") quotes.
-Then paste the result of running 'node passwordGenerator' inside the student's "password" field in the database and apply.
+Then paste the result of running 'node passwordGenerator' inside the student's "password" field in the database and apply those changes.
 
 You can also use this generator to create the password by using update(generateOTP()) instead:*/
 function generateOTP() {
